32d3ff32ec0477aacfface868b8cfee197f19b65
biroman <eivind-buodd@hotmail.com>
2025-01-19 22:42:57 +0100

    fixed scroll to top

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,17 @@
 import "./App.css";
-import { useLayoutEffect } from "react";
-import { About } from "./pages/about";
-import { Contact } from "./pages/contact";
-import { Work } from "./pages/work";
-import { Msscraper } from "./pages/msscraper";
-import { Msdashboard } from "./pages/msdashboard";
-import { Easyplay } from "./pages/easyplay";
-import { Chatlogs } from "./pages/chatlogs";
-import { Routes, Route, Navigate, useLocation } from "react-router-dom";
+import { useEffect } from "react";
+import { useLocation } from "react-router-dom";
 import NavBar from "./components/nav";
 import Footer from "./components/footer";
 import Layout from "./components/layout";
 
-const Wrapper = ({ children }) => {
-  const location = useLocation();
+const Wrapper = () => {
+  const { pathname } = useLocation();
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
 
-  useLayoutEffect(() => {
-    // Scroll to the top of the page when the route changes
-    window.scrollTo({ top: 0, left: 0, behavior: "instant" });
-  }, [location.pathname]);
-
-  return children;
+  return null;
 };
 
 function App() {
@@ -29,22 +20,10 @@ function App() {
       <div className="flex items-center justify-center h-screen w-screen text-[#999999] text-[14px]">
         <div className="container mx-auto mb-0 px-44 h-screen">
           <section className="h-full grid grid-cols-1">
-            <Wrapper>
-              <NavBar />
-              <Layout>
-                <Routes>
-                  <Route path="/" element={<Navigate to="/work" replace />} />
-                  <Route path="/work" element={<Work />} />
-                  <Route path="/about" element={<About />} />
-                  <Route path="/contact" element={<Contact />} />
-                  <Route path="/msscraper" element={<Msscraper />} />
-                  <Route path="/msdashboard" element={<Msdashboard />} />
-                  <Route path="/easyplay" element={<Easyplay />} />
-                  <Route path="/chatlogs" element={<Chatlogs />} />
-                </Routes>
-              </Layout>
-              <Footer />
-            </Wrapper>
+            <Wrapper />
+            <NavBar />
+            <Layout />
+            <Footer />
           </section>
         </div>
       </div>
